9921a13926bb825606c04caeaf1967deb5f8041d
José <joavila@unal.edu.co>
2024-08-02 17:04:23 -0500

    Changed to run on port 5173

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/frontendsolicitudes/', // Ensure this matches your repository name
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000 // or any port you prefer
-  }
+    port: 5173 // or any port you prefer
+  },
+  build: {
+    outDir: 'build', // Ensure this matches the directory used in the deploy script
+  },
 });
